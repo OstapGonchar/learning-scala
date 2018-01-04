172d6b49e7c3d9dcccd80e2bb2800fe0f393f54c
Ostap Gonchar <ostap.gonchar@gmail.com>
2018-01-04 14:13:21 +0200

    Page 244 - Improvement added

--- src/main/scala/Element.scala
+++ src/main/scala/Element.scala
@@ -7,15 +7,37 @@ abstract class Element {
 
   def width: Int = if (height == 0) 0 else contents(0).length
 
-  def above(that: Element): Element =
-    elem(this.contents ++ that.contents)
+  def above(that: Element): Element = {
+    val this1 = this widen that.width
+    val that1 = that widen this.width
+    elem(this1.contents ++ that1.contents)
+  }
 
-  def beside(that: Element): Element =
+  def beside(that: Element): Element = {
+    val this1 = this heighten that.height
+    val that1 = that heighten this.height
     elem(
       for (
-        (line1, line2) <- this.contents zip that.contents
+        (line1, line2) <- this1.contents zip that1.contents
       ) yield line1 + line2
     )
+  }
+
+  def widen(w: Int): Element =
+    if (w <= width) this
+    else {
+      val left = elem(' ', (w - width) / 2, height)
+      val right = elem(' ', w - width - left.width, height)
+      left beside this beside right
+    }
+
+  def heighten(h: Int): Element =
+    if (h <= height) this
+    else {
+      val top = elem(' ', width, (h - height) / 2)
+      val bot = elem(' ', width, h - height - top.height)
+      top above this above bot
+    }
 
   override def toString: String = contents mkString "\n"
 }
@@ -23,7 +45,8 @@ abstract class Element {
 object Element {
 
   private class ArrayElement(val contents: Array[String]) extends Element {
-    require(if (height != 0) contents.map(_.length).forall(_ == contents.head.length) else true)
+    require(if (height != 0) contents.map(_.length).forall(_ == contents.head.length) else true,
+      "Width should be same for all elements")
   }
 
   private class LineElement(s: String) extends Element {
@@ -36,10 +59,10 @@ object Element {
   }
 
   private class UniformElement(
-                        ch: Char,
-                        override val width: Int,
-                        override val height: Int
-                      ) extends Element {
+                                ch: Char,
+                                override val width: Int,
+                                override val height: Int
+                              ) extends Element {
     private val line = ch.toString * width
 
     def contents: Array[String] = Array.fill(height)(line)
--- src/main/scala/Learning.scala
+++ src/main/scala/Learning.scala
@@ -1,8 +1,16 @@
-
+import Element.elem
 
 object Learning extends App {
 
-  private val arrayElement = new ArrayElement(Array("test"))
+  private val arrayElement = elem(Array("test"))
 
   println(s"Height is ${arrayElement.height} and width is ${arrayElement.width}")
+
+  private val diffWidths: Element = elem(Array("hello")) above elem(Array("world!"))
+
+  println(diffWidths)
+
+  private val diffHeights: Element = elem(Array("one", "two")) beside elem(Array("one"))
+
+  println(diffHeights)
 }
